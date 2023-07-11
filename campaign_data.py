5ae524a564c309d7efe4dfef63978faddf195ced
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-07-11 17:29:22 +0530

    fixed device-model, and state-country mappings. bug fixes 🐛

--- campaign_data.py
+++ campaign_data.py
@@ -1,24 +1,41 @@
+""" Campaign Data file """
 import csv
-from datetime import datetime, timedelta
+from datetime import (
+    datetime,
+    timedelta
+)
 import random
 import yaml
 
 class CampaignDataGenerator:
+    """
+    Class to generate and store campaign data
+    """
     def __init__(self, start_date, end_date, config_file):
         self.start_date = start_date
         self.end_date = end_date
         self.config = self.load_config(config_file)
+        self.country_states = self.config['states']
+        self.models = self.config['models']
         self.header = self.config['header']
         self.data = []
 
     def load_config(self, config_file):
-        with open(config_file, 'r') as file:
+        """
+        Method to read YAML file and load configs
+        
+        :param config_file: config file path
+        """
+        with open(config_file, 'r', encoding='utf-8') as file:
             config = yaml.safe_load(file)
         return config
 
-    def generate_data(self, file_path):
+    def generate_data(self):
+        """
+        Method to generate random campaign ad data
+        """
         current_date = self.start_date
-        
+
         while current_date <= self.end_date:
             month_name = current_date.strftime('%b %Y')
             utc_date = current_date.strftime("%Y-%m-%d")
@@ -26,22 +43,25 @@ class CampaignDataGenerator:
             traffic_source = random.choice(self.config['traffic_sources'])
             account_name = random.choice(self.config['account_names'])
             campaign_manager = random.choice(self.config['campaign_managers'])
-            device_viewed = random.choice(self.config['devices'])
-            device_models = random.choice(self.config['device_models'])
             template_name = random.choice(self.config['template_names'])
+
+            device_viewed = random.choice(self.config['devices'])
+            device_models = self.models[device_viewed]
+            device_model = random.choice(device_models)
+
             country = random.choice(self.config['countries'])
-            state = random.choice(self.config['states'])
-            city = random.choice(self.config['cities'])
+            states = self.country_states[country]
+            state = random.choice(states)
 
             browser = random.choice(self.config['browsers'])
             campaign_url = f"https://www.example.com/{template_name.lower().replace(' ', '-')}"
 
             if random.random() < 0.2:
                 impressions = random.randint(10, 50)
-                page_clicks = random.randint(1, impressions)
+                page_clicks = random.randint(10, impressions)
                 revenue = page_clicks * random.uniform(0.5, 2)
-                blocked_clicks = random.randint(0, 2)
-                ad_spend = random.randint(10, 100)
+                blocked_clicks = random.randint(0, 10)
+                ad_spend = random.uniform(10.00, 100.00)
                 paid_clicks = page_clicks - blocked_clicks
                 count_of_ads_returned = random.randint(1, 2)
                 roas = revenue / ad_spend
@@ -50,11 +70,11 @@ class CampaignDataGenerator:
                 cpa = ad_spend / paid_clicks if paid_clicks > 0 else 0
                 gross_profit = revenue - ad_spend
             else:
-                impressions = random.randint(100, 500)
-                page_clicks = random.randint(1, impressions)
+                impressions = random.randint(300, 800)
+                page_clicks = random.randint(100, impressions)
                 revenue = page_clicks * random.uniform(2, 5)
-                blocked_clicks = random.randint(10, 100)
-                ad_spend = random.randint(1000, 5000)
+                blocked_clicks = random.randint(50, 100)
+                ad_spend = random.uniform(200.00, 600.00)
                 paid_clicks = page_clicks - blocked_clicks
                 count_of_ads_returned = random.randint(2, 5)
                 roas = revenue / ad_spend
@@ -64,28 +84,39 @@ class CampaignDataGenerator:
                 gross_profit = revenue - ad_spend
 
             row = [
-                utc_date, traffic_source, account_name, campaign_manager, device_viewed, device_models,
-                browser,
-                month_name, template_name, country, state, city,
-                campaign_url, impressions, page_clicks, revenue, blocked_clicks, ad_spend, paid_clicks, 
-                count_of_ads_returned, roas, rcp, cpc, cpa, gross_profit
+                utc_date, traffic_source, account_name, campaign_manager,
+                device_viewed, device_model, browser, month_name,
+                template_name, country, state, campaign_url,
+                impressions, page_clicks, revenue, blocked_clicks,
+                ad_spend, paid_clicks, count_of_ads_returned, roas,
+                rcp, cpc, cpa, gross_profit
             ]
 
             self.data.append(row)
 
             current_date += timedelta(days=1)
-        self.write_csv(file_path, self.data)
+        self.write_csv(self.data)
 
-    def write_csv(self, file_path, data):
-        with open(file_path, 'w', newline='') as file:
+    def write_csv(self, data):
+        """
+        Method to write data to CSV file
+        
+        :param data: Random Ads Data
+        """
+        with open(
+            self.config['file_path'][0],
+            'w', 
+            newline='',
+            encoding='utf-8'
+        ) as file:
             writer = csv.writer(file)
             writer.writerow(self.header)
             writer.writerows(data)
 
-# Usage example
-start_date = datetime(2018, 1, 1)
-end_date = datetime(2022, 12, 31)
-config_file = 'configs/configs.yaml'
+# Input
+START_DATE = datetime(2018, 1, 1)
+END_DATE = datetime(2022, 12, 31)
+CONFIG_FILE = 'configs/configs.yaml'
 
-generator = CampaignDataGenerator(start_date, end_date, config_file)
-generator.generate_data('data/campaign_data.csv')
+generator = CampaignDataGenerator(START_DATE, END_DATE, CONFIG_FILE)
+generator.generate_data()

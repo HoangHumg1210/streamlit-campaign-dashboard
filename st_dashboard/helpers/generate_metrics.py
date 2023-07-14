be0ff4a539eed8673b866d0ef078029c428b28bc
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-07-14 17:31:23 +0530

    streamlit metrics cards

--- st_dashboard/helpers/generate_metrics.py
+++ st_dashboard/helpers/generate_metrics.py
@@ -1,12 +1,26 @@
 """Code for Metrics"""
-import pandas as pd
 
-FILE_PATH = 'data/campaign_data.csv'
+def get_metrics(data_frame, st_obj):
 
-def get_metrics(st):
-    """Return streamlit metrics"""
-    col1, col2, col3, col4 = st.columns(4)
-    col1.metric("Avg. Impressions", "785k")
-    col2.metric("Avg. CTR", "77.58%")
-    col3.metric("Avg. Gross Profit", "87547k")
-    col4.metric("Avg. RPC", "1.90")
+    """
+    Streamlit metrics cards
+
+    :param data_frame: Pandas dataframe
+    :param st_obj: Object for streamlit component
+    """
+
+    impressions = data_frame.loc[:, 'impressions'].sum()
+    clicks = data_frame.loc[:, 'page_clicks'].sum()
+    ctr = round(
+        100.0 * data_frame.loc[:, 'page_clicks'].sum() / data_frame.loc[:, 'impressions'].sum()
+    , 2
+    )
+    gross_profit = round(data_frame.loc[:, 'gross_profit'].sum(), 2)
+    avg_rpc = round(data_frame.loc[:, 'rpc'].mean(), 2)
+
+    col1, col2, col3, col4, col5 = st_obj.columns(5)
+    col1.metric("Total Impressions", impressions)
+    col2.metric("Total Clicks", clicks)
+    col3.metric("CTR", f"{ctr}%")
+    col4.metric("Total Gross Profit", gross_profit)
+    col5.metric("Total RPC", f"{avg_rpc}")

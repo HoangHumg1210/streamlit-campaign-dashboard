4af9cae0b2009df4586683938c3c0556d414df9c
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-07-14 17:30:55 +0530

    removed All data and Individual campaign dropdown

--- st_dashboard/dashboard.py
+++ st_dashboard/dashboard.py
@@ -10,101 +10,65 @@ st.set_page_config(layout='wide')
 st.title('Marketing Dashboard 📊')
 
 FILE_PATH = 'data/campaign_data.csv'
-df = pd.read_csv(FILE_PATH, parse_dates=[0, 1], infer_datetime_format=True)
+df = pd.read_csv(FILE_PATH, parse_dates=[0])
 
-# Campaign Manager Filter
-option = st.selectbox(
-    'Select Campaign Level Analysis',
-    ('All Data', 'Campaign Manager level'))
+min_date = min(df.utc_date)
+max_date = max(df.utc_date)
 
-if option == 'All Data':
-    st.text('Showing All time campaign stats')
-    get_metrics(st)
+with st.form(key='filters'):
+    cm, platform, dfilter = st.columns(3)
 
-elif option == 'Campaign Manager level':
-
-    min_date = min(df.utc_date)
-    max_date = max(df.utc_date)
-
-    with st.form(key='filters'):
-        cm, platform, dfilter, tfilter = st.columns(4)
-
-        CM_NAME = cm.selectbox(
-            'Select Campaign Manager Name',
-            (
-                'Campaign Manager 1', 
-                'Campaign Manager 2', 
-                'Campaign Manager 3', 
-                'Campaign Manager 4',
-                'Campaign Manager 5'
-            )
+    CM_NAME = cm.selectbox(
+        'Select Campaign Manager Name',
+        (
+            'Campaign Manager 1', 
+            'Campaign Manager 2', 
+            'Campaign Manager 3', 
+            'Campaign Manager 4',
+            'Campaign Manager 5'
         )
+    )
 
-        CM_Platform = platform.selectbox(
-            'Select Campaign Source (eg: Facebook Ads, etc.)',
-            (
-                'Facebook Ads',
-                'LinkedIn Ads',
-                'YouTube Ads',
-                'Reddit Ads',
-                'TikTok Ads',
-                'Instagram Ads',
-                'Twitter Ads'
-            )
+    CM_Platform = platform.selectbox(
+        'Select Campaign Source (eg: Facebook Ads, etc.)',
+        (
+            'Facebook Ads',
+            'LinkedIn Ads',
+            'YouTube Ads',
+            'Reddit Ads',
+            'TikTok Ads',
+            'Instagram Ads',
+            'Twitter Ads'
         )
+    )
 
-        date_filter = dfilter.date_input(
-            "Pick a Date Range",
-            (min_date, max_date)
+    date_filter = dfilter.date_input(
+        "Pick a Date Range",
+        (min_date, max_date)
+    )
+
+    start_date = pd.to_datetime(date_filter[0], format='%Y-%m-%d')
+    end_date = pd.to_datetime(date_filter[1], format='%Y-%m-%d')
+
+    submitted = st.form_submit_button("Submit")
+
+if submitted:
+
+    df_filter = df[
+        (df['campaign_manager'] == CM_NAME) &
+        (df['traffic_source'] == CM_Platform) &
+        (df['utc_date'] >= start_date) &
+        (df['utc_date'] <= end_date)
+    ]
+
+    get_metrics(df_filter, st)
+
+    with st.spinner('Generating Report....'):
+        st.write(df_filter)
+        st.plotly_chart(
+            generate_imp_vs_clks(df_filter[
+                ['utc_date', 'impressions', 'page_clicks']
+            ]),
+            use_container_width=True
         )
-
-        template_used = tfilter.selectbox(
-            'Select Template Name (eg: Flash Deal, Summer Sale, etc)',
-            (
-                'Flash Deal',
-                'Summer Sale',
-                'Product Demo',
-                'B2B Networking Event',
-                'Professional Development Course'
-            )
-        )
-
-        start_date = pd.to_datetime(date_filter[0], format='%Y-%m-%d')
-        end_date = pd.to_datetime(date_filter[1], format='%Y-%m-%d')
-
-        submitted = st.form_submit_button("Submit")
-
-    if submitted:
-
-        df_filter = df[
-            (df['campaign_manager'] == CM_NAME) &
-            (df['traffic_source'] == CM_Platform) &
-            (df['utc_date'] >= start_date) &
-            (df['utc_date'] <= end_date) &
-            (df['template_name'] == template_used)
-        ]
-
-        with st.spinner('Generating Report....'):
-            st.write(df_filter)
-            # generate_imp_vs_clks(
-                # df_filter[
-                #     ['year_month', 'impressions', 'page_clicks']
-                # ]
-            # )
-            st.plotly_chart(
-                generate_imp_vs_clks(df_filter[
-                    ['year_month', 'impressions', 'page_clicks']
-                ]),
-                use_container_width=True
-            )
-            st.balloons()
-
-    # get_metrics(st)
-
-# elif option == 'Category':
-#     st.text('Please Select at least 1 Category to view analysis')
-#     get_metrics(st)
-
-# else:
-#     st.text('Please Select at least 1 Traffic Source to view analysis')
-#     get_metrics(st)
+        st.balloons()
